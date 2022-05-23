e21269d6392f00ab8476f7e9fe8d03330fd9e629
Boluwatife2904 <sanvicola2000@gmail.com>
2022-05-23 17:32:46 +0100

    chore: implement UI for Error 404 pages

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -3,14 +3,27 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <div class="error-404">
-        <h4 class="error-404__heading">Page Not Found!!!!</h4>
-        <p class="error-404__paragraph">This page not found;
+    <div class="error-404 text-center">
+        <h4 class="error-404__heading heading-1">404 ERROR</h4>
+        <p class="error-404__paragraph heading-3">This page not found; <br>
             back to home and start again
         </p>
-        <router-link :to="{ name: 'index'}"></router-link>
+        <router-link :to="{ name: 'index'}" class="button button--medium button--outline-black">HOMEPAGE</router-link>
     </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+.error-404 {
+    margin: 20rem auto;
+    max-width: 43.6rem;
+
+    &__heading {
+        margin-bottom: 2.4rem;
+    }
+
+    &__paragraph {
+        color: var(--dark-gray);
+        margin-bottom: 6.4rem;
+    }
+}
 </style>
